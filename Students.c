eab681700efab00313feeecf4f6f112907f25516
Ashok-max-it <ashokdollin87@gmail.com>
2025-05-13 15:43:45 +0530

    Create Students.c

---
+++ Students.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+int siet ()
+{
+printf ("ashok:name");
+printf (" DOB:28/12/2006");
+printf ("phone number:9113071287");
+printf ("usn:1SV24CS016");
+}
+int main()
+{
+    siet();
+    return 0;
+}
+
