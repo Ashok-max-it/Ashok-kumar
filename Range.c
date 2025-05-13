b2c357ff4c0e8802978dbffb405d5e3fbd4bff1b
Ashok-max-it <ashokdollin87@gmail.com>
2025-05-13 18:54:33 +0530

    Create Range.c

---
+++ Range.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main() {
+    int i;
+    printf("Numbers from 1 to 100:\n");
+    for(i = 1; i <= 100; i++) {
+        printf("%d ", i);
+    }
+
+    printf("\n");
+    return 0;
+}
